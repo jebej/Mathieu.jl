81d60acc727e82cb7e523129b029577488336a59
Jeremy <jeremy.bejanin@gmail.com>
2018-03-27 16:03:25 -0400

    Simplified/faster function and derivative calculations

--- src/integerorder_derivs.jl
+++ src/integerorder_derivs.jl
@@ -1,35 +1,31 @@
 function cep_prime(n::Order,q,z,N::Int=matsize3(n,q))
     An = cep_coeffs(n,q,N) # Calculate coefficients
-    ce = zeros(length(z),length(n))
-    for j = 1:length(n), k = 0:N-1 # Calculate function at z, for all n
-        ce[:,j] .-= An[k+1,j].*2k.*sin.(2k.*z)
-    end
-    return ce
+    # Evaluate sine for all z values
+    C = [-2k*sin(2k*z) for z in z, k in 0:N-1]
+    # Calculate function at z, for all n
+    return C * An
 end
 
 function cea_prime(n::Order,q,z,N::Int=matsize3(n,q))
     An = cea_coeffs(n,q,N) # Calculate coefficients
-    ce = zeros(length(z),length(n))
-    for j = 1:length(n), k = 0:N-1 # Calculate function at z, for all n
-        ce[:,j] .-= An[k+1,j].*(2k+1).*sin.((2k+1).*z)
-    end
-    return ce
+    # Evaluate sine for all z values
+    C = [-(2k+1)*sin((2k+1)*z) for z in z, k in 0:N-1]
+    # Calculate function at z, for all n
+    return C * An
 end
 
 function sea_prime(n::Order,q,z,N::Int=matsize3(n,q))
     Bn = sea_coeffs(n,q,N) # Calculate coefficients
-    se = zeros(length(z),length(n))
-    for j = 1:length(n), k = 0:N-1 # Calculate function at z, for all n
-        se[:,j] .+= Bn[k+1,j].*(2k+1).*cos.((2k+1).*z)
-    end
-    return se
+    # Evaluate cosine for all z values
+    S = [(2k+1)*cos((2k+1)*z) for z in z, k in 0:N-1]
+    # Calculate function at z, for all n
+    return S * Bn
 end
 
 function sep_prime(n::Order,q,z,N::Int=matsize3(n,q))
     Bn = sep_coeffs(n,q,N) # Calculate coefficients
-    se = zeros(length(z),length(n))
-    for j = 1:length(n), k = 0:N-1 # Calculate function at z, for all n
-        se[:,j] .+= Bn[k+1,j].*(2k+2).*cos.((2k+2).*z)
-    end
-    return se
+    # Evaluate cosine for all z values
+    S = [(2k+2)*cos((2k+2)*z) for z in z, k in 0:N-1]
+    # Calculate function at z, for all n
+    return S * Bn
 end
--- src/integerorder_funs.jl
+++ src/integerorder_funs.jl
@@ -1,37 +1,33 @@
 function cep(n::Order,q,z,N::Int=matsize3(n,q))
     An = cep_coeffs(n,q,N) # Calculate coefficients
-    ce = zeros(length(z),length(n))
-    for j = 1:length(n), k = 0:N-1 # Calculate function at z, for all n
-        ce[:,j] .+= An[k+1,j].*cos.(2k.*z)
-    end
-    return ce
+    # Evaluate cosine for all z values
+    C = [cos(2k*z) for z in z, k in 0:N-1]
+    # Calculate function at z, for all n
+    return C * An
 end
 
 function cea(n::Order,q,z,N::Int=matsize3(n,q))
     An = cea_coeffs(n,q,N) # Calculate coefficients
-    ce = zeros(length(z),length(n))
-    for j = 1:length(n), k = 0:N-1 # Calculate function at z, for all n
-        ce[:,j] .+= An[k+1,j].*cos.((2k+1).*z)
-    end
-    return ce
+    # Evaluate cosine for all z values
+    C = [cos((2k+1)*z) for z in z, k in 0:N-1]
+    # Calculate function at z, for all n
+    return C * An
 end
 
 function sea(n::Order,q,z,N::Int=matsize3(n,q))
     Bn = sea_coeffs(n,q,N) # Calculate coefficients
-    se = zeros(length(z),length(n))
-    for j = 1:length(n), k = 0:N-1 # Calculate function at z, for all n
-        se[:,j] .+= Bn[k+1,j].*sin.((2k+1).*z)
-    end
-    return se
+    # Evaluate sine for all z values
+    S = [sin((2k+1)*z) for z in z, k in 0:N-1]
+    # Calculate function at z, for all n
+    return S * Bn
 end
 
 function sep(n::Order,q,z,N::Int=matsize3(n,q))
     Bn = sep_coeffs(n,q,N) # Calculate coefficients
-    se = zeros(length(z),length(n))
-    for j = 1:length(n), k = 0:N-1 # Calculate function at z, for all n
-        se[:,j] .+= Bn[k+1,j].*sin.((2k+2).*z)
-    end
-    return se
+    # Evaluate sine for all z values
+    S = [sin((2k+2)*z) for z in z, k in 0:N-1]
+    # Calculate function at z, for all n
+    return S * Bn
 end
 
 function fun_per(m::Order,q,z,N::Int=matsize3(n,q))
